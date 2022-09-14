9c1863c72e50d1f5ce7cda4e75667db4db42661c
pauli <pauline.bergerot@hotmail.fr>
2022-09-14 22:47:34 +0200

    ajout de l'enum strategy des attributs de la classe Student

---
+++ src/lo02/cestdubrutal/enums/Strategy.java
@@ -0,0 +1,5 @@
+package lo02.cestdubrutal.enums;
+
+public enum Strategy {
+    defensive(),offensive(),random()
+}
--- src/lo02/cestdubrutal/objects/Student.java
+++ src/lo02/cestdubrutal/objects/Student.java
@@ -1,10 +1,17 @@
 package lo02.cestdubrutal.objects;
 
+import lo02.cestdubrutal.enums.Strategy;
+
 public class Student {
 
     private Player player;
     private int creditECTS;
     private int strength = 0;
+    private int dexterity;
+    private int resistance;
+    private int constitution;
+    private int initiative;
+    private Strategy strategy;
 
     public Student() {
 
