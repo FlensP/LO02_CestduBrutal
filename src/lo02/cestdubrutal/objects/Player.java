29a4b5538ae536b6c0771b278b422db46af16530
pauli <pauline.bergerot@hotmail.fr>
2022-09-19 17:14:57 +0200

    add getter setter pointsLeft in Player

--- src/lo02/cestdubrutal/objects/Player.java
+++ src/lo02/cestdubrutal/objects/Player.java
@@ -9,6 +9,8 @@ public class Player {
     private final ArrayList<Student> students = new ArrayList<>();
     private final Program program;
 
+    private int pointsLeft = 0;
+
     public Player(Program program) {
         this.program = program;
     }
@@ -21,11 +23,19 @@ public class Player {
         return students;
     }
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         students.add(student);
     }
 
-    public void removeStudent(Student student){
+    public void removeStudent(Student student) {
         students.remove(student);
+    }
+
+    public int getPointsLeft() {
+        return pointsLeft;
+    }
+
+    public void setPointsLeft(int pointsLeft) {
+        this.pointsLeft = pointsLeft;
     }
 }
